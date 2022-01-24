bf9adb7014e3f61e89dd0144968bfd4978421c49
KlausNottbohm <klaus.nottbohm@sap.com>
2022-01-24 17:58:11 +0100

    rel V220124

--- OverdueDocs.js
+++ OverdueDocs.js
@@ -1,7 +1,7 @@
 
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-const conVersion = "V220121-2";
+const conVersion = "V220124";
 
 const conDocArchURL = "https://docarchive.azurewebsites.net/app/document";
 
