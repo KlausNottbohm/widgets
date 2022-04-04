6aa1060035257df3d79a7092f85ba23fdd520f9d
KlausNottbohm <klaus.nottbohm@sap.com>
2022-04-04 18:12:20 +0200

    rel V220404 tankstelle

--- Tankstellenpreise.js
+++ Tankstellenpreise.js
@@ -1,49 +1,68 @@
-// Version 1.1.5
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-purple; icon-glyph: magic;
+// V220404
 // https://github.com/Necriso/ScriptableWidgets/blob/main/tankstellenpreise.js
 // Check www.scriptables.net for more widgets
 // Use www.scriptdu.de to keep the widget up-to-date
 // Usage:
-// Add credentials toyour widget parameters: 
+// Add credentials toyour widget parameters:
 // API-Key|radius in km|fixedLocation (0 or 1) e.g my-api-key|1|0
 // If you want to set a fixed location then the settings should look like:
-// API-Key|radius in km|fixedLocation|latitude|longitude (0 or 1) e.g my-api-key|1|1|54.322|10.1355
+// radius in km|fixedLocation|latitude|longitude (0 or 1) e.g my-api-key|1|1|54.322|10.1355
 // Important: Don't set the radius to big, the tankerkoenig.de endpoint will deliver all stations in the radius which is set,
 // but only one is needed to display, so it will take a long time to fetch data.
 
-let apiKey, radius, fixedLocation, latitude, longitude, myLocation, brand
+let radius, fixedLocation, latitude, longitude, myLocation, brand
+const conRadius = 5;
 let widgetInput = args.widgetParameter;
-apiKey = "ea6a2788-eb7c-d68d-914e-b11a471d56cb";
+let apiKey = "ea6a2788-eb7c-d68d-914e-b11a471d56cb";
 
 if (widgetInput !== null) {
-    [apiKey, radius, fixedLocation, latitude, longitude, brand] = widgetInput.split("|");
+    [radius, fixedLocation, latitude, longitude, brand] = widgetInput.split("|");
+    console.table([radius, fixedLocation, latitude, longitude, brand]);
 
-    if (!apiKey || !radius || !fixedLocation) {
-        throw new Error("Invalid parameter. Expected format: apiKey|radius (1-20)|fixedLocation (0 or 1)")
-    }
-    if (fixedLocation === 1 && (!latitude || !longitude)) {
-        throw new Error("If fixed location is set to 1 you must set latitude and longitude")
+    if (fixedLocation && (!latitude || !longitude)) {
+        throw new Error("If fixed location is set you must set latitude and longitude")
     }
 
     // Set strings to correct types
-    radius = parseInt(radius)
-    fixedLocation = parseInt(fixedLocation)
+    radius = radius ? parseInt(radius) : conRadius;
+    console.log(`radius ${radius}`);
 
     if (!brand) {
         brand = false
     }
-    if (fixedLocation == 0) {
-        myLocation = {
-            latitude: 0,
-            longitude: 0
+    if (!fixedLocation) {
+        Location.setAccuracyToThreeKilometers();
+        let location;
+        try {
+            location = await Location.current();
+            console.log('get current lat/lon ' + location?.latitude + " " + location?.longitude);
+        } catch (e) {
+            console.log('using saved lat/lon ' + location?.latitude + " " + location?.longitude);
+            location = getSavedIncidenceLatLon();
         }
+        myLocation = location;
     } else {
         myLocation = {
             latitude: parseFloat(latitude),
             longitude: parseFloat(longitude)
         }
     }
-} else {
-    throw new Error("No Widget paramter set. Expected format: apiKey|radius (1-20)|fixedLocation (0 or 1)")
+}
+else {
+    radius = conRadius;
+    Location.setAccuracyToThreeKilometers();
+    let location;
+    try {
+        location = await Location.current();
+        console.log('get current lat/lon ' + location?.latitude + " " + location?.longitude);
+    } catch (e) {
+        console.log('using saved lat/lon ' + location?.latitude + " " + location?.longitude);
+        location = getSavedIncidenceLatLon();
+    }
+    myLocation = location;
 }
 
 const backColor = Color.dynamic(new Color('FFFFFF'), new Color('111111'));
@@ -56,7 +75,7 @@ let station = await loadStation(apiKey, radius, fixedLocation, myLocation)
 let widget = await createWidget(station, brand)
 
 if (!config.runsInWidget) {
-    await widget.presentSmall()
+    await widget.presentMedium()
 }
 
 Script.setWidget(widget)
@@ -70,22 +89,39 @@ async function loadStation(apiKey, radius, fixedLocation, myLocation) {
     } else {
         location = await Location.current()
     }
-
-    const data = await new Request(apiURL(location, radius, apiKey)).loadJSON()
+    let myURL = apiURL(location, radius, apiKey);
+    console.log(myURL)
+    const data = await new Request(myURL).loadJSON();
 
     if (data.stations.length === 0) {
         return { error: 1 }
     }
+    //console.log("");
+    //console.log("vor sort");
+    //console.log(data);
+    data.stations.sort(function (left, right) { return left.diesel - right.diesel; });
+    //console.log("");
+    //console.log("nach sort");
+    //console.log(data);
     return data
 }
+
+const getSavedIncidenceLatLon = () => {
+    //let { fm, path } = getPath();
+    //let data = fm.readString(path);
+    //return JSON.parse(data);
+    const myArgs = "49.0,8.5";
+
+    return getLocationFromString(myArgs);
+};
 
 function formatValue(value) {
     if (!value) {
         return '-'
     }
-    let lastDigit = '?'
-    let price = value.toString().slice(0, -1)
-    return price + lastDigit + "�"
+    let price = value.toFixed(2);
+
+    return price + "€"
 }
 
 async function createWidget(data, brand) {
@@ -119,10 +155,12 @@ async function createWidget(data, brand) {
 
     const attr = selectedStations[0]
 
-    let open = '??'
-    if (attr.isOpen) {
-        open = '??'
-    }
+    let open = attr.isOpen ? `open` : 'closed'
+
+    let myTitleStack = list.addStack();
+    let myTitle = myTitleStack.addText(`Günstigste Tankstelle im Umkreis von ${radius} km`);
+    myTitle.font = Font.boldRoundedSystemFont(12)
+    myTitle.textColor = Color.red()
 
     let firstLineStack = list.addStack()
 
@@ -178,13 +216,23 @@ async function createWidget(data, brand) {
 
     let addressStack = list.addStack()
 
-    let station = addressStack.addText(attr.street)
-    station.font = Font.lightSystemFont(12)
-    station.textColor = textColor
+    addAddressPart(attr.place + ", ");
 
-    let houseNumber = addressStack.addText(" " + attr.houseNumber)
-    houseNumber.font = Font.lightSystemFont(12)
-    houseNumber.textColor = textColor
+    addAddressPart(attr.street);
+
+    addAddressPart(" " + attr.houseNumber);
+
+    addAddressPart(` (${attr.dist} km)`);
 
     return list
+
+    function addAddressPart(pText) {
+        addTextToStack(addressStack, pText);
+    }
+
+    function addTextToStack(pAddressStack, pText) {
+        let houseNumber = pAddressStack.addText(pText);
+        houseNumber.font = Font.lightSystemFont(12);
+        houseNumber.textColor = textColor;
+    }
 }
